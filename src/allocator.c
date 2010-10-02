b5c6ab4e101c14cc0ac6a461690aacb789291c5a
LegionarioDeAcero <LegionarioDeAcero@62ac8f2a-d522-2157-0591-9955a9d8fc73>
2010-10-02 04:14:03 +0000

    Ahi le cambie el nombre a mmu por el de allocator

---
+++ src/allocator.c
@@ -0,0 +1,45 @@
+#include "../include/allocator.h"
+
+char page_map [PAGE_QTY/8];	// Page frames information.
+unsigned short byte_ptr = 0;		// Pointer to the first available set of pages with at
+									// least one available page in memory (A BYTE in BITMAP).
+char bit_ptr = 0;					// Index of the first empty bit in a byte (A BIT IN A 'byte_ptr').
+
+
+/* 'binaryPow':
+  		Returns 2 to the power of 'exp'.
+*/
+int binaryPow(int exp){
+	int ans = 2;
+	if (exp == 0) return 1;
+	while(exp-- > 1) ans *= 2;
+	return ans;
+}
+
+/* 'allocator':
+  		Returns a pointer to a free page.
+  		NULL in case there is no free page in memory.
+*/
+void * allocator(){
+	int page_number = (byte_ptr * 8) + bit_ptr;
+	if (page_number == PAGE_QTY) return NULL;	// !!!!! MEMORY IS FULL !!!!!
+
+	page_map[byte_ptr] |= binaryPow(bit_ptr);		// Updates page_map
+
+	/* Updates pointer */
+	if (++bit_ptr == 8){ bit_ptr = 0; byte_ptr++; }
+	
+	return (void *)(page_number * PAGE_SIZE);
+}
+
+/* 'deallocator':
+  		Given a pointer 'ptr', frees its associated page in memory.
+*/ 
+void deallocator(void * ptr){
+	unsigned short page_byte = (unsigned int) ptr / (PAGE_SIZE*8);
+	char page_bit = (((unsigned int) ptr) / PAGE_SIZE) % 8;
+	char present = ~binaryPow(page_bit);
+	page_map[page_byte] &= present;
+}
+
+
